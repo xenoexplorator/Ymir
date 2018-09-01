98f2a41ea6e1fab7b6866ccfc5cb9318b7decca4
Boivin92 <boivin.92@gmail.com>
2018-09-01 09:01:29 -0400

    pillars and some integration

---
+++ Scenes/Arena.gd
@@ -0,0 +1,14 @@
+extends Node
+
+const PLAYER_SCENE_LOAD_PATH = "res://Scenes/Player.tscn"
+const SPAWNER_SCENE_LOAD_PATH = "res://Scenes/EnemySpawner.tscn"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
