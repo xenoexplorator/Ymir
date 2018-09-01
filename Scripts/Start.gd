010b26e3bec11b7920cf4bf29d1ba79af4932bce
Boivin92 <boivin.92@gmail.com>
2018-08-31 21:08:56 -0400

    All scene transitions

--- Scripts/Start.gd
+++ Scripts/Start.gd
@@ -4,7 +4,10 @@ extends Button
 # var a = 2
 # var b = "textvar"<
 const ARENA_SCENE_PATH = "res://Scenes/Arena.tscn"
-const TUTORIAL_SCENE_PATH = ""
+const TUTORIAL_SCENE_PATH = "res://Scenes/Tutorial.tscn"
+const MAINMENU_SCENE_PATH = "res://MainMenu.tscn"
+
+signal startGame
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -12,6 +15,7 @@ func _ready():
 	pass
 
 func _on_Start_button_up():
+	emit_signal("startGame")
 	get_tree().change_scene(ARENA_SCENE_PATH)
 	
 func _on_Tutorial_button_up():
@@ -19,3 +23,6 @@ func _on_Tutorial_button_up():
 	
 func _on_Quit_button_up():
 	get_tree().quit()
+
+func _on_MainMenu_button_up():
+	get_tree().change_scene(MAINMENU_SCENE_PATH)
